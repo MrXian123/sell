0d32afff60ad44deabfa1083823efff4d4d1c84b
xkjtx <1144983347@qq.com>
2019-05-28 00:27:35 +0800

    测试

--- src/main/java/com/xkj/enums/ResultEnum.java
+++ src/main/java/com/xkj/enums/ResultEnum.java
@@ -10,6 +10,8 @@ public enum ResultEnum {
 
     PRODUCT_NOT_EXIST(10,"商品不存在"),
     PRODUCT_STOCK_ERROR(11,"商品库存不正确"),
+    PRODUCT_STOCK_SUCCESS(11,"商品库存正确"),
+
     ;
 
     private Integer code;
